63bf132dd019a83afa3e165388e5e703ecd3f535
Faniriantsoa RANDRIAHARIMINO <frandriaharimino@yahoo.com>
2023-05-20 19:05:47 +0300

    Tous les animations page d'accueil + fichier JS

---
+++ script/script.js
@@ -0,0 +1,7 @@
+let submitBtnForm = document.getElementById("submitBtnForm")
+
+let popupBox = document.getElementById("popupBox")
+
+submitBtnForm.addEventListener("click", () => {
+        popupBox.style.display = "block"
+    })
