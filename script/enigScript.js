f24465359697c548236b5612c709734dfed96a99
Faniriantsoa RANDRIAHARIMINO <frandriaharimino@yahoo.com>
2023-05-22 19:55:08 +0300

    Amélioration responsivité

---
+++ script/enigScript.js
@@ -0,0 +1,7 @@
+let EnigSubmitBtnForm = document.getElementById("EnigSubmitBtnForm")
+
+let EnigPopupBox = document.getElementById("EnigPopupBox")
+
+EnigSubmitBtnForm.addEventListener("click", () => {
+        EnigPopupBox.style.display = "block"
+    })
